14174ddaf9872b8e7dc97f9f5bb64f17aa14a237
Maria Victoria Giraudo <vickygiraudo@gmail.com>
2023-11-13 23:06:53 -0300

    Pre Entrega 1 - Giraudo Maria Victoria, corrección del número del carrito

--- src/Components/CartWidgte/CartWidget.jsx
+++ src/Components/CartWidgte/CartWidget.jsx
@@ -2,8 +2,9 @@
 const CartWidget = () => {
   return (
     <div>
-        <button>
+        <button className="botonCarrito btn nowrap">
             <img src="img/carritoblanco.jpg" alt="imágen de carrito para poder comprar por tienda virtual" width={60}/>
+            <h4>0</h4>
         </button>
     </div>
   )
